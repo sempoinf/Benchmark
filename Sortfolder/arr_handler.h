e801c7ab4d3ed309d37ab6b8cead7ef4920c02e4
sempoinf <91320209+sempoinf@users.noreply.github.com>
2021-11-21 17:36:54 +0300

    Create arr_handler.h

---
+++ Sortfolder/arr_handler.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+int* arr_gen(int);
+void merge_sort(int*, int, int);
+void selection_sort(int*, int);
+void single_array(void);
+void benchmark(void);

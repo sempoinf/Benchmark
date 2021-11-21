f3cc7a5334002e22a8ffa3e80efe2a68516958be
sempoinf <91320209+sempoinf@users.noreply.github.com>
2021-11-21 17:48:49 +0300

    Create HW4.c

---
+++ HW4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include "libs/arr_handler.h"
+
+int main(void) {
+    short flag = 0;
+    printf("If you want to run benchmark, enter 1\nIf you want to sort a single array, enter 2\n");
+    scanf("%hd", &flag);
+
+    if (flag == 1)
+        benchmark();
+    else if (flag == 2)
+        single_array();
+
+    return 0;
+}

de54a9a1bb3c3c4886cc220cf370d36abecb9656
sempoinf <91320209+sempoinf@users.noreply.github.com>
2021-11-21 17:47:36 +0300

    Update plots.py

--- plots.py
+++ plots.py
@@ -1,16 +1,16 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-df = pd.read_csv('/home/eugeneknvlv/flita/results.csv')
+df = pd.read_csv('/home/sempoinf/project3/results.csv')
 
 fig, (ax1, ax2, ax3) = plt.subplots(3)
 
-ax1.plot(df[['size']], df[['merge sort']], 'r')
-ax2.plot(df[['size']], df[['selection sort']], 'b') 
-ax3.plot(df[['size']], df[['merge sort']], 'r', df[['size']], df[['selection sort']], 'b')
+ax1.plot(df[['size']], df[['quick sort']], 'r')
+ax2.plot(df[['size']], df[['counting sort']], 'b') 
+ax3.plot(df[['size']], df[['quick sort']], 'r', df[['size']], df[['counting sort']], 'b')
 
-ax1.set_title('Merge sort')
-ax2.set_title('Selection sort')
+ax1.set_title('Quick sort')
+ax2.set_title('Counting sort')
 ax3.set_title('Comparison')
 
 plt.tight_layout()

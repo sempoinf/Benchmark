b088e700aa544b88b6eb368b3a7337da6795954e
sempoinf <91320209+sempoinf@users.noreply.github.com>
2021-11-22 21:41:37 +0300

    Update arr_handler.h

--- Sortfolder/arr_handler.h
+++ Sortfolder/arr_handler.h
@@ -6,7 +6,7 @@
 #include <stdio.h>
 
 int* arr_gen(int);
-void merge_sort(int*, int, int);
-void selection_sort(int*, int);
+void quick_sort(int*, int, int);
+void counting_sort(int*, int);
 void single_array(void);
 void benchmark(void);
